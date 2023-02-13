12b2b35a3034d38d35654a5f02ed5e5eeecb7e69
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-13 13:37:38 -0300

    Horas
    js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const formatarDigito = (digito) => `0${digito}`. slice(-2);
 const atualizar = (tempo) => {
     const segundos = document.getElementById('segundos');
     const minutos = document.getElementById('minutos');
+    const horas = document.getElementById('horas');
 
     const qtdSegundos = tempo % 60;
     const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
+    const qtdHoras = Math.floor((tempo % (60 * 60 * 24)) / (60 * 60 ));
 
     segundos.textContent = formatarDigito( qtdSegundos);
     minutos.textContent = formatarDigito( qtdMinutos);
+    horas.textContent = formatarDigito( qtdHoras);
 }
 
 const contagemRegressiva= (tempo) => {
@@ -24,4 +27,4 @@ const contagemRegressiva= (tempo) => {
     const id = setInterval(contar,1000);
 }
 
-contagemRegressiva(92);
+contagemRegressiva(11200);
