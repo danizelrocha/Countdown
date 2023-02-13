a1e1f1a0c684e37638f322f28faddbba63135564
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-13 13:25:47 -0300

    minutos
    js

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@ const formatarDigito = (digito) => `0${digito}`. slice(-2);
 
 const atualizar = (tempo) => {
     const segundos = document.getElementById('segundos');
+    const minutos = document.getElementById('minutos');
 
     const qtdSegundos = tempo % 60;
+    const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
 
     segundos.textContent = formatarDigito( qtdSegundos);
+    minutos.textContent = formatarDigito( qtdMinutos);
 }
 
 const contagemRegressiva= (tempo) => {
     const pararContagem = () => clearInterval(id);
 
     const contar = () => {
-        if (tempo == 0){
+        if (tempo === 0){
             pararContagem();
         }
         atualizar (tempo);
@@ -21,4 +24,4 @@ const contagemRegressiva= (tempo) => {
     const id = setInterval(contar,1000);
 }
 
-contagemRegressiva(15);
+contagemRegressiva(92);
